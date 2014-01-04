cc6b0b3b19530eab1d2978f52ffdaabe3ab26171
take_cheeze <takechi101010@gmail.com>
2014-01-05 00:55:02 +0900

    use oniguruma api directly

--- src/mruby_onig_regexp.c
+++ src/mruby_onig_regexp.c
@@ -7,18 +7,11 @@
 #include <mruby/string.h>
 #include <mruby/data.h>
 #include <mruby/variable.h>
-#include "onigposix.h"
-
-struct mrb_onig_regexp {
-  regex_t re;
-  int flag;
-};
+#include "oniguruma.h"
 
 static void
 onig_regexp_free(mrb_state *mrb, void *p) {
-  struct mrb_onig_regexp *pre = (struct mrb_onig_regexp *) p;
-  regfree(&pre->re);
-  mrb_free(mrb, pre);
+  onig_free((OnigRegex) p);
 }
 
 static struct mrb_data_type mrb_onig_regexp_type = {
@@ -27,49 +20,41 @@ static struct mrb_data_type mrb_onig_regexp_type = {
 
 static void
 onig_regexp_init(mrb_state *mrb, mrb_value self, mrb_value str, mrb_value flag) {
-  mrb_value regexp;
-  struct mrb_onig_regexp *reg;
-
-  regexp = mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, "@regexp"));
-  if (mrb_nil_p(regexp)) {
-    reg = malloc(sizeof(struct mrb_onig_regexp));
-    memset(reg, 0, sizeof(struct mrb_onig_regexp));
-    mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, "@regexp"), mrb_obj_value(
-        Data_Wrap_Struct(mrb, mrb->object_class,
-          &mrb_onig_regexp_type, (void*) reg)));
-  }else{
-    Data_Get_Struct(mrb, regexp, &mrb_onig_regexp_type, reg);
-    regfree(&reg->re);
+  if (!mrb_nil_p(mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, "@regexp")))) {
+    mrb_raise(mrb, E_RUNTIME_ERROR, "regexp already specified");
   }
 
   int cflag = 0;
-  if (mrb_nil_p(flag)) {
-    cflag = REG_EXTENDED | REG_NEWLINE;
-  } else if (mrb_fixnum_p(flag)) {
-    int nflag = mrb_fixnum(flag);
-    if (nflag & 1) cflag |= REG_ICASE;
-    if (nflag & 2) cflag |= REG_EXTENDED;
-    if (nflag & 4) cflag |= REG_NEWLINE;
-  } else if (mrb_type(flag) == MRB_TT_TRUE)
-    cflag |= REG_ICASE;
-  else if (mrb_string_p(flag)) {
-    if (RSTRING_LEN(flag) == 0) {
-      cflag = REG_EXTENDED | REG_NEWLINE;
-    } else {
-      if (strchr(RSTRING_PTR(flag), 'i')) cflag |= REG_ICASE;
-      if (strchr(RSTRING_PTR(flag), 'x')) cflag |= REG_EXTENDED;
-      if (strchr(RSTRING_PTR(flag), 'm')) cflag |= REG_NEWLINE;
-    }
+  OnigSyntaxType* syntax = ONIG_SYNTAX_RUBY;
+  if(mrb_nil_p(flag)) {
+  } else if(mrb_type(flag) == MRB_TT_TRUE) {
+    cflag |= ONIG_OPTION_IGNORECASE;
+  } else if(mrb_fixnum_p(flag)) {
+    int int_flags = mrb_fixnum(flag);
+    if(int_flags & 0x1) { cflag |= ONIG_OPTION_IGNORECASE; }
+    if(int_flags & 0x2) { cflag |= ONIG_OPTION_EXTEND; }
+    if(int_flags & 0x4) { cflag |= ONIG_OPTION_MULTILINE; }
+  } else if(mrb_string_p(flag)) {
+    char const* str_flags = RSTRING_PTR(flag);
+    if(strchr(str_flags, 'i')) { cflag |= ONIG_OPTION_IGNORECASE; }
+    if(strchr(str_flags, 'x')) { cflag |= ONIG_OPTION_EXTEND; }
+    if(strchr(str_flags, 'm')) { cflag |= ONIG_OPTION_MULTILINE; }
+  } else {
+    mrb_raisef(mrb, E_ARGUMENT_ERROR, "unknown regexp flag: %S", flag);
   }
-  reg->flag = cflag;
-  int regerr = regcomp(&reg->re, RSTRING_PTR(str), cflag);
-  if (regerr) {
-    char err[256];
-    regerror(regerr, &reg->re, err, sizeof(err));
+
+  OnigErrorInfo einfo;
+  OnigRegex reg;
+  if (onig_new(&reg, (OnigUChar*)RSTRING_PTR(str), (OnigUChar*) RSTRING_PTR(str) + RSTRING_LEN(str),
+               cflag, ONIG_ENCODING_UTF8, syntax, &einfo) != ONIG_NORMAL) {
     mrb_raisef(mrb, E_ARGUMENT_ERROR, "'%S' is an invalid regular expression because %S.",
-      str, mrb_str_new_cstr(mrb, err));
+               str, mrb_str_new(mrb, (char const*)einfo.par, einfo.par_end - einfo.par));
   }
   mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, "@source"), str);
+
+  mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, "@regexp"), mrb_obj_value(
+      Data_Wrap_Struct(mrb, mrb->object_class,
+                       &mrb_onig_regexp_type, reg)));
 }
 
 static mrb_value
@@ -81,62 +66,62 @@ onig_regexp_initialize(mrb_state *mrb, mrb_value self) {
   return mrb_nil_value();
 }
 
+struct traverse_arg {
+  mrb_state* mrb;
+  mrb_value result;
+  int arena_idx;
+};
+
 static mrb_value
 onig_regexp_match(mrb_state *mrb, mrb_value self) {
-  const char *str;
+  mrb_value str;
   mrb_value regexp;
-  struct mrb_onig_regexp *reg;
+  OnigRegex reg;
   mrb_int pos = 0;
 
-  mrb_get_args(mrb, "z|i", &str, &pos);
-  if (pos < 0 || pos >= strlen(str)) {
+  mrb_get_args(mrb, "S|i", &str, &pos);
+  if (pos < 0 || pos >= RSTRING_LEN(str)) {
     return mrb_nil_value();
   }
 
-  regexp = mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, "@regexp"));
+  regexp = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@regexp"));
   Data_Get_Struct(mrb, regexp, &mrb_onig_regexp_type, reg);
 
-  int i;
-  size_t nmatch = 999;
-  regmatch_t match[nmatch];
-  for (i = 0; i < nmatch; i++)
-    match[i].rm_so = -1;
-  int regerr = regexec(&reg->re, str + pos, nmatch, match, 0);
-  if (regerr == REG_NOMATCH)
+  OnigRegion* match = onig_region_new();
+  OnigUChar const* str_ptr = (OnigUChar*)RSTRING_PTR(str);
+  int match_result = onig_search(reg, str_ptr, str_ptr + RSTRING_LEN(str),
+                                 str_ptr + pos, str_ptr + RSTRING_LEN(str), match, 0);
+  if (match_result == ONIG_MISMATCH) {
+    onig_region_free(match, 0);
     return mrb_nil_value();
-
-  if (regerr != 0) {
-    char err[256];
-    regerror(regerr, &reg->re, err, sizeof(err));
-    mrb_raisef(mrb, E_ARGUMENT_ERROR, "'%S' is an invalid regular expression because %S.",
-      mrb_str_new_cstr(mrb, str), mrb_str_new_cstr(mrb, err));
+  } else if (match_result < 0) {
+    char err[ONIG_MAX_ERROR_MESSAGE_LEN] = "";
+    onig_error_code_to_str((OnigUChar*)err, match_result);
+    mrb_raise(mrb, E_REGEXP_ERROR, err);
   }
 
-  mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, "@last_match"), mrb_nil_value());
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@last_match"), mrb_nil_value());
+
+  mrb_value c = mrb_obj_new(mrb, mrb_class_get(mrb, "OnigMatchData"), 0, NULL);
+  mrb_iv_set(mrb, c, mrb_intern_lit(mrb, "@string"), mrb_str_dup(mrb, str));
 
   int ai = mrb_gc_arena_save(mrb);
-  struct RClass* clazz;
-  clazz = mrb_class_get(mrb, "OnigMatchData");
-  mrb_value c = mrb_obj_new(mrb, clazz, 0, NULL);
-  mrb_iv_set(mrb, c, mrb_intern_cstr(mrb, "@string"), mrb_str_new_cstr(mrb, str));
-  mrb_value args[2];
-  for (i = 0; i < nmatch; i++) {
-    if (match[i].rm_so != -1) {
-      args[0] = mrb_fixnum_value(match[i].rm_so + pos);
-      args[1] = mrb_fixnum_value(match[i].rm_eo - match[i].rm_so);
-      mrb_funcall_argv(mrb, c, mrb_intern_cstr(mrb, "push"), sizeof(args)/sizeof(args[0]), &args[0]);
-      mrb_gc_arena_restore(mrb, ai);
-    }
+  int i;
+  for(i = 0; i < match->num_regs; ++i) {
+    mrb_value func_args[] = { mrb_fixnum_value(match->beg[i]), mrb_fixnum_value(match->end[i] - match->beg[i]) };
+    mrb_funcall_argv(mrb, c, mrb_intern_lit(mrb, "push"), 2, func_args);
+    mrb_gc_arena_restore(mrb, ai);
   }
 
   mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, "@last_match"), c);
+  onig_region_free(match, 0);
   return c;
 }
 
 static mrb_value
 onig_regexp_equal(mrb_state *mrb, mrb_value self) {
   mrb_value other, regexp_self, regexp_other;
-  struct mrb_onig_regexp *self_reg, *other_reg;
+  OnigRegex self_reg, other_reg;
 
   mrb_get_args(mrb, "o", &other);
   if (mrb_obj_equal(mrb, self, other)){
@@ -153,7 +138,7 @@ onig_regexp_equal(mrb_state *mrb, mrb_value self) {
   if (!self_reg || !other_reg){
       mrb_raise(mrb, E_RUNTIME_ERROR, "Invalid OnigRegexp");
   }
-  if (self_reg->flag != other_reg->flag){
+  if (onig_get_options(self_reg) != onig_get_options(other_reg)){
       return mrb_false_value();
   }
   return mrb_str_equal(mrb, mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, "@source")), mrb_iv_get(mrb, other, mrb_intern_cstr(mrb, "@source"))) ?
@@ -163,11 +148,11 @@ onig_regexp_equal(mrb_state *mrb, mrb_value self) {
 static mrb_value
 onig_regexp_casefold_p(mrb_state *mrb, mrb_value self) {
   mrb_value regexp;
-  struct mrb_onig_regexp *reg;
+  OnigRegex reg;
 
   regexp = mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, "@regexp"));
   Data_Get_Struct(mrb, regexp, &mrb_onig_regexp_type, reg);
-  return (reg->flag & REG_ICASE) ? mrb_true_value() : mrb_false_value();
+  return (onig_get_options(reg) & ONIG_OPTION_IGNORECASE) ? mrb_true_value() : mrb_false_value();
 }
 
 void
@@ -188,6 +173,7 @@ mrb_mruby_onig_regexp_gem_init(mrb_state* mrb) {
 
 void
 mrb_mruby_onig_regexp_gem_final(mrb_state* mrb) {
+  (void)mrb;
 }
 
 // vim:set et:
