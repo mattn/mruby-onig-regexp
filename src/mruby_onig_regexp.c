3a288ef34aecd42a5faface4851c2e417f01cadd
take_cheeze <takechi101010@gmail.com>
2014-02-16 00:52:21 +0900

    suppress warning of cast and remove unnecessary unary plus

--- src/mruby_onig_regexp.c
+++ src/mruby_onig_regexp.c
@@ -478,7 +478,7 @@ string_scan(mrb_state* mrb, mrb_value self) {
   Data_Get_Struct(mrb, match_expr, &mrb_onig_regexp_type, reg);
   mrb_value const result = mrb_nil_p(blk)? mrb_ary_new(mrb) : self;
   mrb_value m_value = create_onig_region(mrb, self, match_expr);
-  OnigRegion* const m = (OnigRegex*)DATA_PTR(m_value);
+  OnigRegion* const m = (OnigRegion*)DATA_PTR(m_value);
   int last_end_pos = 0;
   int i;
 
@@ -548,7 +548,7 @@ string_split(mrb_state* mrb, mrb_value self) {
       last_end_pos += 1;
     } else {
       mrb_ary_push(mrb, result, mrb_str_substr(
-          mrb, self, + last_end_pos, match->beg[0] - last_end_pos));
+          mrb, self, last_end_pos, match->beg[0] - last_end_pos));
       last_end_pos = match->end[0];
     }
   }
