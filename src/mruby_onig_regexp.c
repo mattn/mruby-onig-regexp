e4c67f6a6fdfb0493eda4621f10fbd53b821f34e
takahashim <maki@rubycolor.org>
2015-09-25 13:01:44 +0900

    fix signature of regexp_expr_str()
    regexp_expr_str() should be used by onig_regexp_to_s() (Regexp#to_s),
    but source string in the function is C string, not mrb_value.

--- src/mruby_onig_regexp.c
+++ src/mruby_onig_regexp.c
@@ -228,11 +228,11 @@ option_to_str(char str[4], int options) {
 }
 
 static mrb_value
-regexp_expr_str(mrb_state *mrb, mrb_value str, mrb_value src) {
-  const char *p, *pend;
+regexp_expr_str(mrb_state *mrb, mrb_value str, const char *p, int len) {
+  const char *pend;
   char buf[5];
 
-  p = RSTRING_PTR(src); pend = RSTRING_END(src);
+  pend = (const char *) p + len;
   for (;p < pend; p++) {
     unsigned char c, cc;
 
@@ -276,7 +276,8 @@ onig_regexp_inspect(mrb_state *mrb, mrb_value self) {
   OnigRegex reg;
   Data_Get_Struct(mrb, self, &mrb_onig_regexp_type, reg);
   mrb_value str = mrb_str_new_lit(mrb, "/");
-  regexp_expr_str(mrb, str, mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@source")));
+  mrb_value src = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@source"));
+  regexp_expr_str(mrb, str, (const char *)RSTRING_PTR(src), RSTRING_LEN(src));
   mrb_str_cat_lit(mrb, str, "/");
   char opts[4];
   if (*option_to_str(opts, onig_get_options(reg))) {
